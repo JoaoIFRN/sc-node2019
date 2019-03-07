86f0435b6a5206f52f823bbabdcd9e49706adee1
Joao Nascimento <joao.nascimento@ifrn.edu.br>
2019-03-07 11:08:54 -0300

    Modificações em app.js

--- api-rest/src/app.js
+++ api-rest/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const Status = require("http-status");
+//const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false}));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log("Está funcionando!");
@@ -12,3 +14,10 @@ app.listen(3000,()=>{
 
 app.use("/",require("./routers/index"));
 app.use("/livros",require("./routers/livros"));
+app.use((req, res, next) => {
+    res.status(Status.NOT_FOUND).send();
+});
+app.use((error,req, res, next) => {
+    res.status(Status.INTERNAL_SERVER_ERROR).json({error});
+});
+
